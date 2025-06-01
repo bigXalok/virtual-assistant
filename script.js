629ee901c390a581444af4b192107dd66783183e
Alok Kumar <aarongangwar@gmail.com>
2025-06-01 23:35:32 +0530

    Your updated javascript

--- script.js
+++ script.js
@@ -29,10 +29,9 @@ const speechRecognition = window.SpeechRecognition || window.webkitSpeechRecogni
 const recognition = new speechRecognition();
 
 recognition.onresult = (event) => {
-  const currentIndex = event.resultIndex;
-  const transcript = event.results[currentIndex][0].transcript;
-  content.innerText = transcript;
-  takeCommand(transcript.toLowerCase());
+  const message = event.results[event.resultIndex][0].transcript.toLowerCase();
+  content.innerText = message;
+  takeCommand(message);
 };
 
 function toggleUI(listening) {
@@ -50,38 +49,44 @@ function takeCommand(message) {
 
   if (message.includes("hello alina") || message.includes("hey alina")) {
     speak("Hello my master, what can I help you with?");
+
   } else if (message.includes("who are you")) {
-    speak("I am a virtual assistant, created by Alok.");
+    speak("I am a virtual assistant Alina, created by Alok.");
+
   } else if (message.includes("open youtube")) {
     speak("Opening YouTube...");
     window.open("https://youtube.com/", "_blank");
+
   } else if (message.includes("open google")) {
     speak("Opening Google...");
     window.open("https://google.com/", "_blank");
-  } else if (message.includes("open facebook")) {
-    speak("Opening Facebook...");
-    window.open("https://facebook.com/", "_blank");
+
   } else if (message.includes("open instagram")) {
     speak("Opening Instagram...");
     window.open("https://instagram.com/", "_blank");
+
   } else if (message.includes("open whatsapp")) {
-    speak("Opening WhatsApp Web...");
-    window.open("https://web.whatsapp.com/", "_blank");
+    speak("Opening WhatsApp");
+    window.open("whatsapp://");
+
   } else if (message.includes("open calculator")) {
-    speak("Opening online calculator...");
+    speak("Opening calculator...");
     window.open("https://www.google.com/search?q=calculator", "_blank");
+
   } else if (message.includes("time")) {
     const time = new Date().toLocaleString(undefined, {
       hour: "numeric",
       minute: "numeric"
     });
     speak(`Current time is ${time}`);
+
   } else if (message.includes("date")) {
     const date = new Date().toLocaleString(undefined, {
       day: "numeric",
       month: "short"
     });
     speak(`Today's date is ${date}`);
+
   } else {
     const query = message.replace("alina", "").trim();
     const finalText = `This is what I found on the internet regarding ${query}`;
